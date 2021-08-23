16df5ebdcb63e4865a750f077171ad4fa6a313fe
Derek V <DVogel345@gmail.com>
2021-08-23 19:39:18 -0400

    exam

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,67 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
-
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
-
-// We'll use these variables to track the counts of each cookie type
-let gb = 0      // Ginger bread
-let cc = 0      // Chocolate Chip
-let sugar = 0   // Sugar Sprinkle
+let yourName = "Derek Vogel" // HINT: Replace this with your own name!
 
 // Code to update name display 
 document.getElementById('credit').textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Ginger Bread cookies
-document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Ginger bread + button was clicked!')
+let incrementBtnOne = document.getElementById('add-gb');
+let decrementBtnOne = document.getElementById('minus-gb');
+let gbCount = document.getElementById('qty-gb');
+let gbCountTotals = document.getElementById('qty-total');
 
-    // TODO: Write the code to be run when the "+" button for "Ginger Bread" is clicked
-})
+let incrementBtnTwo = document.getElementById('add-cc');
+let decrementBtnTwo = document.getElementById('minus-cc');
+let ccCount = document.getElementById('qty-cc');
+let ccCountTotals = document.getElementById('qty-total');
 
-// TODO: Hook up event listeners for the rest of the buttons
+let incrementBtnThree = document.getElementById('add-sugar');
+let decrementBtnThree = document.getElementById('minus-suagr');
+let sugarCount = document.getElementById('qty-sugar');
+let sugarCountTotals = document.getElementById('qty-total');
+
+let totalCookiesValue = 0;
+
+
+increment(incrementBtnOne, gbCount, gbCountTotals);
+decrement(decrementBtnOne, gbCount, gbCountTotals);
+
+increment(incrementBtnTwo, ccCount, ccCountTotals);
+decrement(decrementBtnTwo, ccCount, ccCountTotals);
+
+increment(incrementBtnThree, sugarCount, sugarCountTotals);
+decrement(decrementBtnThree, sugarCount, sugarCountTotals);
+
+function increment(button, input, total) {   
+  button.addEventListener("click", function(event) {
+    num = parseInt(input.innerHTML);
+    // console.log("input.innerHTML " + input.innerHTML);
+    input.innerHTML = num + 1;
+    total.innerHTML = input.innerHTML;
+    getTotal();
+  });
+};
+
+function decrement(button, input, total) {   
+  button.addEventListener("click", function(event) {
+    num = parseInt(input.innerHTML);
+    // console.log("input.innerHTML " + input.innerHTML);
+    if (num == 0) {
+      return;
+    }
+    input.innerHTML = num - 1;
+    total.innerHTML = input.innerHTML;
+    getTotal();
+  });  
+};
+
+function getTotal() {
+  let inputFiled = document.querySelectorAll(".input-filed");
+  let totalCookies = document.getElementById("qty-total");
+  let totalCookiesValue = 0;
+  
+  for (let i = 0; i < inputFiled.length; i++) {
+    totalCookiesValue += parseInt(inputFiled[i].innerHTML);
+    // console.log("totalCookies?: " + totalCookies.innerHTML); 
+  }
+  totalCookies.innerHTML = totalCookiesValue;
+}
